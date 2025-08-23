25fffac2dec70913f7bfa71e1cae019ee423e982
Jason Edward Salim <jasonedwardsalim@gmail.com>
2025-08-23 11:25:31 +0700

    refactor: Card onClick link

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -22,7 +22,10 @@ export default function Card(props: {
             day: "numeric",
           })}
       </p>
-      <div className="xl:w-96 lg:w-72 lg:h-64 md:w-48 md:h-64 w-36  h-48 relative text-black flex items-end hover:-translate-y-2 hover:cursor-pointer transition-all duration-200">
+      <Link
+        href={`/acara/${titleToSlug(title)}`}
+        className="xl:w-96 lg:w-72 lg:h-64 md:w-48 cursor-pointer md:h-64 w-36  h-48 relative text-black flex items-end hover:-translate-y-2 transition-all duration-200"
+      >
         <Image
           src={coverUrl}
           className="rounded-2xl z-0 object-cover"
@@ -30,13 +33,13 @@ export default function Card(props: {
           fill
           unoptimized
         />
-        <Link href={`/acara/${titleToSlug(title)}`} className="w-full ">
-          <div className="relative cursor-pointer font-playfair px-2 md:px-6 max-md:pb-6 lg:px-12 text-md lg:text-2xl flex justify-between items-end md:items-center text-[#FDFF78] z-10 h-24 bg-gradient-to-t from-[#002492] via-[rgba(0,36,146,0.83)] via-70% to-transparent rounded-b-2xl w-full">
+        <div className="w-full ">
+          <div className="relative  font-playfair px-2 md:px-6 max-md:pb-6 lg:px-12 text-md lg:text-2xl flex justify-between items-end md:items-center text-[#FDFF78] z-10 h-24 bg-gradient-to-t from-[#002492] via-[rgba(0,36,146,0.83)] via-70% to-transparent rounded-b-2xl w-full">
             <h2 className="font-medium">{title}</h2>
             <Play fill="#FDFF78" className="size-4 md:size-6 lg:size-8" />
           </div>
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   );
 }
